a6528c961a5e8a790ce6e2a3565b3162e8012d0a
farrert <farrert@mail.nmc.edu>
2022-05-15 17:07:31 -0400

    first commit

---
+++ Lesson12/modifystyles.js
@@ -0,0 +1,6 @@
+function applyChanges(){
+    document.body.style.background = document.getElementById("backgroundColor").value;
+    document.body.style.color = document.getElementById("textColor").value;
+    document.body.style.fontFamily = document.getElementById("font").value;
+    document.body.style.fontSize = document.getElementById("size").value;
+}
